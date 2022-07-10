ada83592afe645051257faa29892f826d2eb8e98
minwoo129 <key0129mw@naver.com>
2022-07-10 13:41:24 +0900

    ch05: 미들웨어 연결 연습

--- ch05/learn-express/app.js
+++ ch05/learn-express/app.js
@@ -1,8 +1,33 @@
 const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
 const path = require('path');
 
+dotenv.config();
 const app = express();
 app.set('port', process.env.PORT || 3000); // 실행할 포트 설정. 
+
+app.use(morgan('dev'));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false
+    },
+    name: 'session-cookie'
+}));
+
+app.use((req, res, next) => {
+    console.log('모든 요청에 다 실행됩니다.');
+    next();
+});
 
 app.get('/', (req, res) => { // get 요청이 들어올 때 어떤 동작을 할지 적는 부분
     //res.send('Hello, Express'); // get / 요청이 들어올 때 Hello, Express를 전송함
