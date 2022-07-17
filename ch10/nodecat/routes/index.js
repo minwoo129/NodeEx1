e4617458cc1d016e8087e5d5a4f6204e8e47f820
minwoo129 <key0129mw@naver.com>
2022-07-17 20:52:40 +0900

    ch10: nodecat sns api 서버 만들기

--- ch10/nodecat/routes/index.js
+++ ch10/nodecat/routes/index.js
@@ -2,35 +2,80 @@ const express = require('express');
 const axios = require('axios');
 
 const router = express.Router();
+const URL = 'http://localhost:8002/v1';
+axios.defaults.headers.origin = 'http://localhost:4000'; // origin 헤더 추가
 
-router.get('/test', async(req, res, next) => { // 토큰 테스트 라우터
+const request = async(req, api) => {
     try {
-        if(!req.session.jwt) { // 세션에 토큰이 없으면 토큰 발급 시도
+        if(!req.session.jwt) { // 세션에 토큰이 없으면
             const tokenResult = await axios.post('http://localhost:8002/v1/token', {
                 clientSecret: process.env.CLIENT_SECRET
             });
-
-            if(tokenResult.data && tokenResult.data.code == 200) {
-                req.session.jwt = tokenResult.data.token; // 세션에 토큰 저장
-            }
-            else { // 토큰 발급 실패
-                return res.json(tokenResult.data); // 발급 실패 사유 응답
-            }
+            req.session.jwt = tokenResult.data.token; // 세션에 토큰 저장
         }
 
-        const result = await axios.get('http://localhost:8002/v1/test', {
+        return await axios.get(`${URL}${api}`, {
             headers: { authorization: req.session.jwt }
-        });
+        }); // api 요청
+    } catch (err) {
+        if(err.response.status == 419) { // 토큰 만료 시 토큰 재발급 받기
+            delete req.session.jwt;
+            return request(req,api);
+        } // 419 외의 다른 에러면
+        return err.response;
+    }
+};
 
-        return res.json(result.data);
+router.get('/mypost', async(req, res, next) => {
+    try {
+        const result = await request(req, '/posts/my');
+        res.json(result.data);
     } catch (err) {
         console.error(err);
-        if(err.response.status == 419) { // 토큰 만료 시
-            return res.json(err.response.data);
-        }
-
-        return next(err);
+        next(err);
     }
 });
+
+router.get('/search/:hashtag', async(req, res, next) => {
+    try {
+        const result = await request(req, `/posts/hashtag/${encodeURIComponent(req.params.hashtag)}`);
+        res.json(result.data);
+    } catch (err) {
+        if(err.code) {
+            console.error(err);
+            next(err);
+        }
+    }
+})
+
+// router.get('/test', async(req, res, next) => { // 토큰 테스트 라우터
+//     try {
+//         if(!req.session.jwt) { // 세션에 토큰이 없으면 토큰 발급 시도
+//             const tokenResult = await axios.post('http://localhost:8002/v1/token', {
+//                 clientSecret: process.env.CLIENT_SECRET
+//             });
+
+//             if(tokenResult.data && tokenResult.data.code == 200) {
+//                 req.session.jwt = tokenResult.data.token; // 세션에 토큰 저장
+//             }
+//             else { // 토큰 발급 실패
+//                 return res.json(tokenResult.data); // 발급 실패 사유 응답
+//             }
+//         }
+
+//         const result = await axios.get('http://localhost:8002/v1/test', {
+//             headers: { authorization: req.session.jwt }
+//         });
+
+//         return res.json(result.data);
+//     } catch (err) {
+//         console.error(err);
+//         if(err.response.status == 419) { // 토큰 만료 시
+//             return res.json(err.response.data);
+//         }
+
+//         return next(err);
+//     }
+// });
 
 module.exports = router;
