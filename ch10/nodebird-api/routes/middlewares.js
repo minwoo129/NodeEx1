07c9ac75e03ad7471bb7b68304b60cc296d0bcb6
minwoo129 <key0129mw@naver.com>
2022-07-18 23:35:20 +0900

    ch10: nodebird-api, nodecat 사용량 제한 구현하기

--- ch10/nodebird-api/routes/middlewares.js
+++ ch10/nodebird-api/routes/middlewares.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const RateLimit = require('express-rate-limit');
 
 exports.isLoggedIn = (req,res, next) => {
     if(req.isAuthenticated()) next();
@@ -30,4 +31,22 @@ exports.verifyToken = (req, res, next) => {
             message: '유효하지 않은 토큰입니다.'
         })
     }
+}
+
+exports.apiLimiter = new RateLimit({
+    windowMs: (60 * 1000), // 1분
+    max: 10, // 허용 횟수
+    handler(req, res) { // 제한 초과 시 콜백 함수
+        res.status(this.statusCode).json({
+            code: this.statusCode, // 기본값 429(사용량 초과)
+            message: '1분에 한 번만 요청할 수 있습니다.'
+        });
+    }
+});
+
+exports.deprecated = (req, res) => { // 사용하면 안되는 라우터에 붙여줄 미들웨어
+    res.status(410).json({
+        code: 410,
+        message: '새로운 버전이 나왔습니다. 새로운 버전을 사용하세요.'
+    });
 }
--- ch10/nodebird-api/routes/v2.js
+++ ch10/nodebird-api/routes/v2.js
@@ -1,19 +1,15 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 
-const { verifyToken, deprecated } = require('./middlewares');
+const { verifyToken, apiLimiter } = require('./middlewares');
 const { Domain, User, Post, Hashtag } = require('../models');
 
 const router = express.Router();
 
-router.use(deprecated);
-
-// 토큰 발급
-router.post('/token', async(req, res) => {
-    const { clientSecret } = req.body;
-
+router.post('/token', apiLimiter, async (req, res) => {
+    const {clientSecret} = req.body;
     try {
-        const domain = await Domain.findOne({
+        const domain = await Domain.findOne({ 
             where: { clientSecret },
             include: {
                 model: User,
@@ -25,17 +21,17 @@ router.post('/token', async(req, res) => {
             return res.status(401).json({
                 code: 401,
                 message: '등록되지 않은 도메인입니다. 먼저 도메인을 등록하세요.'
-            })
+            });
         }
 
-        const {id, nick} = domain.User;
         const token = jwt.sign({
-            id,
-            nick,
-          }, process.env.JWT_SECRET, {
-            expiresIn: '1m', // 1분
-            issuer: 'nodebird',
-          });
+            id: domain.User.id,
+            nick: domain.User.nick
+        }, process.env.JWT_SECRET, {
+            expiresIn: '30m',
+            issuer: 'nodebird'
+        });
+
         return res.json({
             code: 200,
             message: '토큰이 발급되었습니다.',
@@ -46,37 +42,35 @@ router.post('/token', async(req, res) => {
         return res.status(500).json({
             code: 500,
             message: '서버 에러'
-        })
+        });
     }
 });
 
-// 토큰 테스트
-router.get('/test', verifyToken, (req, res) => {
+router.get('/test', verifyToken, apiLimiter, (req, res) => {
     res.json(req.decoded);
 });
 
-router.get('/posts/my', verifyToken, (req, res) => {
+router.get('/posts/my', apiLimiter, verifyToken, (req, res) => {
     Post.findAll({ where: { userId: req.decoded.id } })
     .then((posts) => {
         console.log(posts);
         res.json({
             code: 200,
             payload: posts
-        });
+        })
     })
-    .catch((error) => {
-        console.error(error);
+    .catch((e) => {
+        console.error(e);
         return res.status(500).json({
             code: 500,
             message: '서버 에러'
         });
-    });
+    })
 });
 
-router.get('/posts/hashtag/:title', verifyToken, async(req, res) => {
+router.get('/posts/hashtag/:title', verifyToken, apiLimiter, async (req, res) => {
     try {
-        const hashtag = await Hashtag.findOne({ where: {title: req.params.title} });
-
+        const hashtag = await Hashtag.findOne({ where: { title: req.params.title } });
         if(!hashtag) {
             return res.status(404).json({
                 code: 404,
@@ -86,15 +80,15 @@ router.get('/posts/hashtag/:title', verifyToken, async(req, res) => {
         const posts = await hashtag.getPosts();
         return res.json({
             code: 200,
-            payload: posts,
+            payload: posts
         });
     } catch (err) {
-        console.error(err);
+        console.error(e);
         return res.status(500).json({
             code: 500,
             message: '서버 에러'
         });
     }
-});
+})
 
 module.exports = router;
