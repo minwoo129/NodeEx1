5e9588e0f679821bcaac11575cefcb130ae017dd
minwoo129 <key0129mw@naver.com>
2022-08-07 14:14:05 +0900

    ch09: 몽고디비 연결설정 수정

--- ch08/learn-mongoose/schemas/index.js
+++ ch08/learn-mongoose/schemas/index.js
@@ -1,23 +1,21 @@
 const mongoose = require('mongoose');
-const {name, password} = require('./connectInfo');
 
 const connect = () => {
     if(process.env.NODE_ENV != 'production') mongoose.set('debug', true);
 
-    mongoose.connect(`mongodb://${name}:${password}@localhost:27017/admin`, {
+    mongoose.connect(`mongodb://rmw:rmwkey0129@localhost:27017/admin`, {
         dbName: 'nodejs',
         useNewUrlParser: true,
-        useCreateIndex: true
     }, (err) => {
         if(err) console.log('몽고디비 연결 에러: ', err);
         else console.log('몽고디비 연결 성공');
     });
 };
 
-mongoose.connect.on('error', (err) => {
+mongoose.connection.on('error', (err) => {
     console.error('몽고디비 연결 에러: ', err)
 });
-mongoose.connect.on('disconnected', () => {
+mongoose.connection.on('disconnected', () => {
     console.error('몽고디비 연결이 끊겼습니다. 연결을 재시도합니다.');
     connect();
 });
